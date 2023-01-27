3845878f8502203098bad1bc8d341ec573d34039
Ikpemosi Arnold <Ikpemosi@protonmail.com>
2023-01-27 07:30:54 +0100

    changes to routes

--- routes/order.js
+++ routes/order.js
@@ -70,12 +70,21 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
 //GET MONTHLY INCOME
 
 router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const productId = req.query.pid;
   const date = new Date();
   const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
   const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
   try {
     const income = await Order.aggregate([
-      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $match: {
+          createdAt: { $gte: previousMonth },
+          ...(productId && {
+            products: { $elemMatch: { productId } },
+          }),
+        },
+      },
       {
         $project: {
           month: { $month: "$createdAt" },
@@ -89,7 +98,6 @@ router.get("/income", verifyTokenAndAdmin, async (req, res) => {
         },
       },
     ]);
-    console.log(income);
     res.status(200).send(income);
   } catch (error) {
     res.status(500).json(error);
